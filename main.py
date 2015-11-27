5d5c68ed181b4f431d755d2fd8c21505d4af9486
bryanp <R3DKoala@hotmail.com>
2015-11-28 10:16:56 +1100

    Added hashing function

---
+++ main.py
@@ -0,0 +1,11 @@
+__author__ = 'bryanp'
+
+import hashlib
+
+def hashfile(afile, hasher, blocksize=65536):
+    buf = afile.read(blocksize)
+    while len(buf) > 0:
+        hasher.update(buf)
+        buf = afile.read(blocksize)
+    return hasher.digest()
+
