19e9fea384dc73b188843a30bdd4f89c50d13342
bryanp <R3DKoala@hotmail.com>
2015-12-01 15:53:45 +1100

    Completed File transfering between client and server, all that is left is file verification

--- client/client.py
+++ client/client.py
@@ -1,31 +1,84 @@
 __author__ = 'bryanp'
 
-import hashlib, os, socket, thread
+import hashlib, socket, zipfile, os, sys, shutil
+
 
 class recieve:
 
     def __init__(self):
+
+        # Delete all old mods
+        try:
+            shutil.rmtree('client/mods')
+            os.mkdir('client/mods')
+        except:
+            os.mkdir('client/mods')
+
         self.s = socket.socket()         # Create a socket object
         self.host = socket.gethostname() # Get local machine name
         self.port = 49491                # Reserve a port for your service.
 
-    # This function recieves the server modlist and hashes (called by peers)
-    # This function will need to recieve a C hook inside the main function in order to get the ip addresses needed
+
+    # This function returns the hash value of a file
+    def hash(self, blocksize=65536):
+
+        hasher = hashlib.sha256()
+        afile = open('client/mods.zip', 'rb')
+        buf = afile.read(blocksize)
+
+        while len(buf) > 0:
+            hasher.update(buf)
+            buf = afile.read(blocksize)
+
+        return hasher.digest()
+
+
     def recieveML(self):
 
-        self.s.connect((self.host, self.port))
+        self.f = open('client/mods.zip','w+')
 
-        print self.s.recv(2048)
+        try:
+            self.s.connect((self.host, self.port))
+        except:
+            print("Couldn't connect to server, quitting")
+            sys.exit()
 
-        self.s.shutdown                     # Close the socket when done
+        print('Recieving server mods\n')
+        l = self.s.recv(1024)
+        while (l):
+            self.f.write(l)
+            l = self.s.recv(1024)
+        self.f.close()
+
+        self.s.close                     # Close the socket when done
+
+
+    def checkhash(self):
+
+        self.Dhash = hash()
+        self.Ohash = 12 #TODO get original hash from server
+
+
+    def extractmods(self):
+
+        print('Extracting server mods\n')
+        with zipfile.ZipFile('client/mods.zip', "r") as z:
+            z.extractall('client/mods')
+
 
 # Main function
 def main():
 
     client = recieve()
     client.recieveML()
+    client.extractmods()
 
 
 # Run the program
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+        print("Server mods recieved\n")
+        os.remove('client/mods.zip')
+    except:
+        os.remove('client/mods.zip')
--- server/server.py
+++ server/server.py
@@ -1,6 +1,6 @@
 __author__ = 'bryanp'
 
-import socket
+import socket,shutil, os, hashlib
 
 class broadcast:
 
@@ -11,25 +11,53 @@ class broadcast:
         self.port = 49491                          # Reserve a port for your service
         self.s.bind((self.host, self.port))        # Bind to the port
 
-    # This function broadcasts the server modlist and hashes to connected peers
+        shutil.make_archive('server/mods', 'zip', 'server/mods')
+
+    # This function returns the hash value of a file
+    def hash(self, blocksize=65536):
+
+        hasher = hashlib.sha256()
+        afile = open('server/mods.zip', 'rb')
+        buf = afile.read(blocksize)
+
+        while len(buf) > 0:
+            hasher.update(buf)
+            buf = afile.read(blocksize)
+
+        afile.close()
+        return hasher.digest()
+
+    # This function broadcasts the server mods and hash to connected peers
     def broadcastML(self):
 
         self.s.listen(5)                           # Now wait for client connection
+        self.f = open('server/mods.zip','rb')
 
         while True:
             self.c, self.addr = self.s.accept()     # Establish connection with client
             print 'Got connection from', self.addr
-            self.c.send('Connected to host\n')
-            self.c.send('Recieving master list of server mods')
+
+            l = self.f.read(1024)
+            while (l):
+                print 'Sending mod files to', self.addr
+                self.c.send(l)
+                l = self.f.read(1024)
+            self.f.close()
+            print 'Done sending mods to', self.addr, '\n'
+
             self.c.close()                          # Close the connection
 
+if __name__ == '__main__':
 
-
-
-def main():
     server = broadcast()
-    print("server is running\n")
-    server.broadcastML()
+    hash = server.hash()
 
-if __name__ == "__main__":
-    main()
+    print('server is running\n')
+
+    try:
+        server.broadcastML()
+        print('File sent\n')
+
+    except:
+        os.remove('server/mods.zip')
+        print('Server shutdown unexpectedly\n')
